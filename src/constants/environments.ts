aa4e5a0c27e9609059dd586ecc677d142c3f48c6
YeabsiraAbr <Yeabsira@kemercode.com>
2024-07-17 23:24:12 +0300

    new change

--- src/constants/environments.ts
+++ src/constants/environments.ts
@@ -1,6 +1,10 @@
 export const environments = {
-  appName: process.env.NEXT_PUBLIC_APP_NAME || '@generic-name',
-  siteUrl: process.env.SITE_URL || (process.env.VERCEL_URL && `https://${process.env.VERCEL_URL}`) || process.env.URL || 'http://localhost:3000',
+  appName: process.env.NEXT_PUBLIC_APP_NAME || "@generic-name",
+  siteUrl:
+    process.env.SITE_URL ||
+    (process.env.VERCEL_URL && `https://yohab-dress.vercel.app`) ||
+    process.env.URL ||
+    "http://localhost:3000",
   backend_url: process.env.NEXT_PUBLIC_BACKEND_URL,
-  analytics: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS
-} 
+  analytics: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
+};
